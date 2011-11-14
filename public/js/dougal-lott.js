12333780f2df9cec2543adffdfd1317acc753731
Jamie White <jamie@jgwhite.co.uk>
2011-11-14 00:23:47 +0000

    Sadly transform js easing is a bit odd

---
+++ public/js/dougal-lott.js
@@ -0,0 +1,10 @@
+$('body').css({ perspective: '1000px' });
+
+$('article').
+mouseenter(function() {
+  // $(this).find('.container').stop().animate({ rotateY: Math.PI }, 600);
+}).
+mouseleave(function() {
+  // $(this).find('.container').stop().animate({ rotateY: 0 }, 600);
+});
+
