40fcedce1c306d109297883afdec9f2aa8ec4133
Jamie White <jamie@jgwhite.co.uk>
2011-11-14 09:04:47 +0000

    Touch-safe

--- public/js/dougal-lott.js
+++ public/js/dougal-lott.js
@@ -1,10 +1,45 @@
-$('body').css({ perspective: '1000px' });
+if (Modernizr.touch) {
 
-$('article').
-mouseenter(function() {
-  // $(this).find('.container').stop().animate({ rotateY: Math.PI }, 600);
-}).
-mouseleave(function() {
-  // $(this).find('.container').stop().animate({ rotateY: 0 }, 600);
-});
+  $('body').bind('touchstart', function(e) {
+    var moved = false;
+
+    var doTap = function() {
+      if (moved) return;
+
+      var target  = $(e.target),
+          article = target.closest('article');
+
+      if (!article) return;
+
+      if (target.attr('href') && !article.hasClass('flipped')) {
+        target.one('click', function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+        });
+      }
+
+      $('article').removeClass('flipped');
+      article.addClass('flipped');
+    }
+
+    $(this).
+    one('touchmove', function() { moved = true }).
+    one('touchend', doTap);
+
+    setTimeout(100, doTap);
+  });
+
+} else if (Modernizr.csstransforms3d) {
+
+  $('article').
+  mouseenter(function() { $(this).addClass('flipped') }).
+  mouseleave(function() { $(this).removeClass('flipped') });
+
+} else {
+
+  $('article').
+  mouseenter(function() { $(this).find('.details').animate({ opacity: 1 }, 'fast') }).
+  mouseleave(function() { $(this).find('.details').animate({ opacity: 0 }) });
+
+}
 
